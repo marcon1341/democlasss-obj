c6daffb0623c5a3b77a5da437df14356c9b7c403
marcon <marconsolomonyadete@gmail.com>
2025-04-15 17:23:02 -0700

    class-object

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    String make;
+    String model;
+    int year;
+
+    //method
+    public void startEngine(){
+        System.out.println(make +" "+model +" " + year + " Is Starting...");
+    }
+}
